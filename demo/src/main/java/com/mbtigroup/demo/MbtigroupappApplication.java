51d4fad0d0cfb2b0f516703757e915aa6dd7f735
ppancake12 <ppenkake@gmail.com>
2022-11-01 13:30:40 +0900

    mbti

---
+++ demo/src/main/java/com/mbtigroup/demo/MbtigroupappApplication.java
@@ -0,0 +1,13 @@
+package com.mbtigroup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtigroupappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtigroupappApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/mbtigroup/demo/MbtigroupappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbtigroup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtigroupappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
